d1f395070db74ade57642616012e9d6139f5a7f5
AuroraLantean <raymondlieu.crypto@runbox.com>
2026-03-23 12:33:27 +0200

    add FormBase component

--- components/FormController.tsx
+++ components/FormController.tsx
@@ -1,3 +1,4 @@
+import { fields } from "@hookform/resolvers/ajv/src/__tests__/__fixtures__/data.js";
 import type { ReactNode } from "react";
 //import { FormBase, FormControlProps } from "./FormBase"
 //import { useFieldContext } from "./hooks"
@@ -20,7 +21,33 @@ type FormControlProps<
   label: ReactNode;
   control: ControllerProps<TFieldValues, TName, TTransformedValues>["control"]; //write ControllerProps here and import it from react-hook-form. copy the generic types from ControllerProps popover hint, paste them into this FormControlProps<...>, and paste the leading generics here ControllerProps<...>
 };
-const FormController = ({ control, name, label }: FormControlProps) => {
+
+type FormBaseProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+  TTransformedValues = TFieldValues,
+> = FormControlProps<TFieldValues, TName, TTransformedValues> & {
+  children: (
+    field: Parameters<
+      ControllerProps<TFieldValues, TName, TTransformedValues>["render"]
+    >[0]["field"] & {
+      "aria-invalid": boolean;
+      id: string;
+    },
+  ) => ReactNode;
+  //horizontal?: boolean
+  //controlFirst?: boolean
+};
+export const FormBase = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+  TTransformedValues = TFieldValues,
+>({
+  children,
+  control,
+  label,
+  name,
+}: FormBaseProps<TFieldValues, TName, TTransformedValues>) => {
   return (
     <Controller
       name={name}
@@ -28,12 +55,27 @@ const FormController = ({ control, name, label }: FormControlProps) => {
       render={({ field, fieldState }) => (
         <Field data-invalid={fieldState.invalid}>
           <FieldLabel htmlFor={field.name}>{label}</FieldLabel>
-          <Input {...field} id={field.name} aria-invalid={fieldState.invalid} />
+          {children({
+            ...field,
+            id: field.name,
+            "aria-invalid": fieldState.invalid,
+          })}
           {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
         </Field>
       )}
     />
   );
+};
+
+//adding generics from above should give hint on name argument... but not working
+const FormController = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+  TTransformedValues = TFieldValues,
+>(
+  props: FormControlProps<TFieldValues, TName, TTransformedValues>,
+) => {
+  return <FormBase {...props}>{(field) => <Input {...field} />}</FormBase>;
 };
 
 export default FormController;
